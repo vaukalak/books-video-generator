2cac71263d0c228b8b230e5c5f0e205d43a118e4
Mikita Belahlazau <nbeloglazov@google.com>
2024-02-26 00:02:42 -0800

    Add preview

--- create-timecodes.ts
+++ create-timecodes.ts
@@ -1,12 +1,10 @@
 #!/usr/bin/env -S npx ts-node --files
 
 import { context } from "./src/context";
-import { getChapterNames } from "./src/getChapterNames";
-import { getTimings } from "./src/getTimings";
+import { getChapters } from "./src/chapters";
 import { formatYoutubeTimecodes } from "./src/format/youtubeTimecodes";
 
 (async () => {
-  const chapters = await getChapterNames(context);
-  const durations = await getTimings(context, chapters);
-  console.log(formatYoutubeTimecodes(chapters, durations));
+  const chapters = await getChapters(context);
+  console.log(formatYoutubeTimecodes(chapters));
 })();
--- src/context.ts
+++ src/context.ts
@@ -9,20 +9,20 @@ if (!book) {
 
 /**
  * YAML config that allows users to customize video.
- * Fields should match sample/config.yml
+ * See sample/config.yml for documentation of what each
+ * field means.
  */
 export interface Config {
-  text_options: {
-    font_size: number,
-    font_color: string,
+  readonly text_options: {
+    font: string;
+    font_file: string;
+    font_size: number;
+    font_color: string;
+    x: string;
+    y: string;
   };
-  background_mode: 'image'|'video';
-}
-
-export interface Chapter {
-  // Index of the chapter.
-  readonly id: number;
-  readonly name: string;
+  readonly background_mode: 'image'|'video';
+  readonly preview_covers: boolean;
 }
 
 /** Timing of a single chapter. */
@@ -31,26 +31,47 @@ export interface Timing {
   readonly startTimeSec: number;
 }
 
+export interface Chapter {
+  /** Index of the chapter. */
+  readonly id: number;
+  readonly name: string;
+  readonly timing: Timing;
+  /** Path to the user-provided audio file for the given chapter in `resource` dir. */
+  readonly sourceAudioFile: string;
+  /** Path to the generated video file in `out` dir. */
+  readonly generatedVideoFile: string;
+}
+
 export interface Context {
   readonly book: string;
+  /** Config provided by the user in config.yml */
   readonly config: Config;
-  renderText: (chapters: Chapter[], chapterIndex: number) => string;
+  /** Directory containing generated video and other files produced by this tool. */
+  readonly outDir: string;
+  renderText: (chapter: Chapter) => string;
   renderBackground: (timing: Timing) => string;
   encoding: string;
 }
 
 function readConfig(book: string): Config {
-  return parse(fs.readFileSync(`resource/${book}/config.yml`).toString())
+  return parse(fs.readFileSync(`resource/${book}/config.yml`, {encoding: 'utf8'}));
 }
 
-export const context: Context = {
-  book,
-  config: readConfig(book),
-  renderText() { 
-    throw new Error('unset'); 
-  },
-  renderBackground() {
-    throw new Error('unset'); 
-  },
-  encoding: '',
-};
+function createContext(book: string): Context {
+  const outDir = `out/${book}`;
+  fs.mkdirSync(outDir,  { recursive: true });
+  return {
+    book,
+    config: readConfig(book),
+    outDir,
+    renderText() { 
+      throw new Error('unset'); 
+    },
+    renderBackground() {
+      throw new Error('unset'); 
+    },
+    encoding: '',
+  };
+}
+
+export const context = createContext(book);
